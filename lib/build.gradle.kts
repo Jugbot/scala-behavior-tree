fc1a00e11247cfd895d919b6dabf675c5b4457ee
Lucas Pollice <lp1935@nyu.edu>
2024-06-06 23:48:41 -0400

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -64,10 +64,10 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://github.com/Jugbot/scala-behavior-tree")
+            url = uri("https://maven.pkg.github.com/Jugbot/scala-behavior-tree")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
